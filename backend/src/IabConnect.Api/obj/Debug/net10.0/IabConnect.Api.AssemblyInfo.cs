dd7d0f6dca1befd14db5c1a3de9f70ba06128f4e
htos <singh.harwinder@outlook.com>
2026-02-07 20:41:20 +0100

    chore: Remove bin/obj folders from git tracking

--- backend/src/IabConnect.Api/obj/Debug/net10.0/IabConnect.Api.AssemblyInfo.cs
+++ backend/src/IabConnect.Api/obj/Debug/net10.0/IabConnect.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IabConnect.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+448d021087e34448868303dcceba78499adf4e8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af390cdf54045d2f201c90e0d9e3e3569220b1f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("IabConnect.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IabConnect.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backend/src/IabConnect.Application/obj/Debug/net10.0/IabConnect.Application.AssemblyInfo.cs
+++ backend/src/IabConnect.Application/obj/Debug/net10.0/IabConnect.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IabConnect.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+448d021087e34448868303dcceba78499adf4e8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af390cdf54045d2f201c90e0d9e3e3569220b1f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("IabConnect.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IabConnect.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
